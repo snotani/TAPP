f16e6dc25583cde8ac2d60abd1c12c52aef6e5c9
croftm <48052088+croftm@users.noreply.github.com>
2019-11-10 06:40:06 +0000

    Update landingPage.dart

--- lib/appScreens/landingPage.dart
+++ lib/appScreens/landingPage.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(LandPage());
 
-  void hello(){
-    print('hello');
-  }
+
 
 class LandPage extends StatefulWidget {
   @override
@@ -16,18 +13,16 @@ class _LandPageState extends State<LandPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TAPP',
-      home: Scaffold(
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-              Text("TAPP", textAlign: TextAlign.center, style: TextStyle(fontSize: 50)),
-                Text('Take action and protect the planet!', textAlign: TextAlign.center, style: TextStyle(fontSize: 20)),
-                Image.asset('lib/images/logo.png'),
-              ])
-
-          )
-    ));
+        title: 'TAPP',
+        home: Scaffold(
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset('lib/images/logo.png'),
+                      Text('Take action and protect the planet!', textAlign: TextAlign.center, style: TextStyle(fontSize: 20)),
+                    ])
+            )
+        ));
   }
 }
